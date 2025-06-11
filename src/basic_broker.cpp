e45b4cdcf38bf1c7dc4e103fea651ba2f270ac92
jarida@bu.edu <jarida@bu.edu>
2025-06-11 14:31:01 -0400

    Fix bug in buy logic

--- src/basic_broker.cpp
+++ src/basic_broker.cpp
@@ -2,24 +2,20 @@
 
 BasicBroker::BasicBroker(double cash) : cash_(cash) {}
 
-void BasicBroker::buy(const Candle& c) {
-    if (cash_ >= c.close) {
+void BasicBroker::buy(const Candle &c) {
+    while (cash_ >= c.close) {
         cash_ -= c.close;
         position_ += 1;
     }
 }
 
-void BasicBroker::sell(const Candle& c) {
+void BasicBroker::sell(const Candle &c) {
     while (position_ > 0) {
         cash_ += c.close;
         position_ -= 1;
     }
 }
 
-double BasicBroker::get_cash() const {
-    return cash_;
-}
+double BasicBroker::get_cash() const { return cash_; }
 
-uint32_t BasicBroker::get_position() const {
-    return position_;
-}
+uint32_t BasicBroker::get_position() const { return position_; }
